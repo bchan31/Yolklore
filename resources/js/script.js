4390957a5f9a3eb330014284a8a6229e0d5a2241
bchan31 <59836699+bchan31@users.noreply.github.com>
2020-03-04 10:02:39 -0600

    first commit

---
+++ resources/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    
+    $('.js--wp-1').waypoint(function(direction){
+            $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+
+
+
+
+
+
+});
