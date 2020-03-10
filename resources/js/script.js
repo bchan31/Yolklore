b77285ee01fa0e693b4ac49f323df7c4f7b92828
bchan31 <59836699+bchan31@users.noreply.github.com>
2020-03-10 14:03:03 -0500

    edited JS for hamburger

--- resources/js/script.js
+++ resources/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
     });
 });
 
-function myFunction() {
+function myNav() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
       x.style.display = "none";
